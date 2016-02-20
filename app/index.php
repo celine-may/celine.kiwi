dbf2e6bdc7637fb88f746862b4c5932f817ea8a8
Celine Rufener <celine.may@gmail.com>
2016-02-20 16:15:47 +1300

    Contact link

--- app/index.php
+++ app/index.php
@@ -40,19 +40,18 @@ include('controllers/' . $controller . 'Controller.php');
   </script>
 </head>
 
-<body class="<?php echo $controller; ?>">
+<body>
 
   <?php include_once('assets/svgs/svg-defs.svg'); ?>
 
   <main class="main">
-    <?php
-      if (is_file("views/$controller.php")) include("views/$controller.php");
-      if (is_file("views/$controller/$view.php")) include("views/$controller/$view.php");
-    ?>
+    <?php if (is_file("views/$controller.php")) include("views/$controller.php"); ?>
   </main>
 
+  <button class="link contact-link">contact</button>
+
   <?php foreach ($assets['javascripts'] as $file_path) : ?>
-    <script src="<?php echo $file_path ?>"></script>
+    <script src="<?php echo $file_path; ?>"></script>
   <?php endforeach; ?>
 </body>
 </html>
--- app/views/home.php
+++ app/views/home.php
@@ -1,4 +1,4 @@
-<section class="section">
+<section class="section home">
   <div class="section-content">
     <span class="logo">
       <svg class="shape-logo">
@@ -6,7 +6,7 @@
       </svg>
     </span>
     <h1 class="title home-title">Celine Rufener</h1>
-    <p class="lead home-lead">Digital Alchemist &amp; Front End Developer</p>
+    <h2 class="lead home-lead">Digital Alchemist &amp; Front End Developer</h2>
 
     <p class="lead home-copy">HTML, CSS &amp; JS are currently being mixed together to create a brand new website. Come back soon to see the result.</p>
   </div>
