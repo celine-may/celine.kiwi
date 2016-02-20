2caea11904491d6dcad947e6d2e7458fce2d98c3
Celine Rufener <celine.may@gmail.com>
2016-02-20 18:56:37 +1300

    git -> tracking dist/.htaccess & dist/config

---
+++ dist/config.php
@@ -0,0 +1,22 @@
+<?php
+
+// Global variables + SMTP init
+define('PATH', '/celine.kiwi/');
+define('EMAIL', 'celine.may@gmail.com');
+define('IMAGES_PATH', PATH . 'assets/images/');
+define('VIDEOS_PATH', PATH . 'assets/videos/');
+
+
+// Errors display
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+// Assets
+$assets = array();
+
+$js_path = PATH.'assets/javascripts/';
+$css_path = PATH.'assets/stylesheets/';
+$assets['javascripts'] = array();
+$assets['stylesheets'] = array(
+  $css_path . 'application.min.css',
+);
