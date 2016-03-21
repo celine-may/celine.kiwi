ba9a54b3a22bc82b5839d3a1e5d78f105e573470
Celine Rufener <celine.may@gmail.com>
2016-03-22 07:28:39 +1300

    Work -> new images

--- app/sections/work.php
+++ app/sections/work.php
@@ -71,7 +71,7 @@
               <li>Responsive</li>
             </ul>
           </div>
-          <a href="http://www.heartkids.co.nz/" class="slide-link btn">view site</a>
+          <a href="http://www.wqsmart.co.nz/" class="slide-link btn">view site</a>
         </div>
       </article>
 
@@ -93,8 +93,57 @@
               <li>Parallax</li>
               <li>Mobile</li>
             </ul>
+            <ul class="slide-list body-copy">
+              <li>W3 Awards Gold</li>
+            </ul>
           </div>
-          <a href="http://www.heartkids.co.nz/" class="slide-link btn">view site</a>
+          <a href="http://www.harrisongrierson.com/" class="slide-link btn">view site</a>
+        </div>
+      </article>
+
+      <article class="slide" data-slide="5">
+        <div class="slide-image">
+          <img src="<?php echo IMAGES_PATH; ?>work/sovereign-rpm.jpg" alt="Preview of Sovereign RPM's website on desktop">
+        </div>
+        <div class="slide-description">
+          <h2 class="slide-title title separator">Sovereign RPM</h2>
+          <div class="slide-copy">
+            <p class="body-copy">
+              Panuku Development Auckland's aim for Wynyard Quarter is to create a mix of residential, retail &amp; commercial development. This website is an engaging way of sharing the progress of implementing sustainability in Wynyard Quarter.
+            </p>
+            <ul class="slide-list body-copy">
+              <li>HTML</li>
+              <li>SASS</li>
+              <li>CoffeeScript</li>
+              <li>AJAX</li>
+              <li>Interactive</li>
+            </ul>
+            <ul class="slide-list body-copy">
+              <li>NZDM Awards Silver</li>
+            </ul>
+          </div>
+        </div>
+      </article>
+
+      <article class="slide" data-slide="6">
+        <div class="slide-image">
+          <img src="<?php echo IMAGES_PATH; ?>work/tvnz-media-portal.jpg" alt="Preview of TVNZ Media Portal's website on desktop">
+        </div>
+        <div class="slide-description">
+          <h2 class="slide-title title separator">TVNZ Media Portal</h2>
+          <div class="slide-copy">
+            <p class="body-copy">
+              Panuku Development Auckland's aim for Wynyard Quarter is to create a mix of residential, retail &amp; commercial development. This website is an engaging way of sharing the progress of implementing sustainability in Wynyard Quarter.
+            </p>
+            <ul class="slide-list body-copy">
+              <li>HTML</li>
+              <li>SASS</li>
+              <li>CoffeeScript</li>
+              <li>AJAX</li>
+              <li>Responsive</li>
+            </ul>
+          </div>
+          <a href="http://www.tvnzmediaportal.co.nz/" class="slide-link btn">view site</a>
         </div>
       </article>
     </div>
