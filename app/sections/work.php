feec94d4aca323d1fea6184eea916657f69d90ff
Celine <celine@methodstudios.co.nz>
2016-03-23 20:11:17 +1300

    slideshow sass as separate file

--- app/sections/work.php
+++ app/sections/work.php
@@ -3,149 +3,151 @@
     <div class="work-bg"></div>
     <h3 class="work-title title separator work-element">Work</h3>
 
-    <div class="slideshow work-element">
-      <article class="slide init" data-slide="1">
-        <div class="slide-image">
-          <img src="<?php echo IMAGES_PATH; ?>work/heart-kids.jpg" alt="Preview of Heart Kids' website on desktop &amp; tablet">
-        </div>
-        <div class="slide-description">
-          <h2 class="slide-title title separator">Heart Kids</h2>
-          <div class="slide-copy">
-            <p class="body-copy">
-              Heart Kids New Zealand's mission is to provide life long support for those affected by congenital heart disease (CHD) and to reach and imrive their lifes. In August 2015, we launch the Born Fighter campaign to create awareness and promote donations to HK NZ.
-            </p>
-            <ul class="slide-list body-copy">
-              <li>HTML</li>
-              <li>SASS</li>
-              <li>CoffeeScript</li>
-              <li>AJAX</li>
-              <li>Parallax</li>
-              <li>Responsive</li>
-            </ul>
+    <div class="work-content work-element">
+      <div class="slideshow">
+        <article class="slide init" data-slide="1">
+          <div class="slide-image">
+            <img src="<?php echo IMAGES_PATH; ?>work/heart-kids.jpg" alt="Preview of Heart Kids' website on desktop &amp; tablet">
           </div>
-          <a href="http://www.heartkids.co.nz/" target="_blank" class="slide-link btn">view site</a>
-        </div>
-      </article>
+          <div class="slide-description">
+            <h2 class="slide-title title separator">Heart Kids</h2>
+            <div class="slide-copy">
+              <p class="body-copy">
+                Heart Kids New Zealand's mission is to provide life long support for those affected by congenital heart disease (CHD) and to reach and imrive their lifes. In August 2015, we launch the Born Fighter campaign to create awareness and promote donations to HK NZ.
+              </p>
+              <ul class="slide-list body-copy">
+                <li>HTML</li>
+                <li>SASS</li>
+                <li>CoffeeScript</li>
+                <li>AJAX</li>
+                <li>Parallax</li>
+                <li>Responsive</li>
+              </ul>
+            </div>
+            <a href="http://www.heartkids.co.nz/" target="_blank" class="slide-link btn">view site</a>
+          </div>
+        </article>
 
-      <article class="slide" data-slide="2">
-        <div class="slide-image">
-          <img src="<?php echo IMAGES_PATH; ?>work/wynyard-quarter.jpg" alt="Preview of Wynyard Quarter's website on desktop &amp; tablet">
-        </div>
-        <div class="slide-description">
-          <h2 class="slide-title title separator">Wynyard Quarter</h2>
-          <div class="slide-copy">
-            <p class="body-copy">
-              The aim of this website is to showcase the vibrant precinct of Wynyard Quarter as well as promoting &amp; attracting visitors and communicating on future developments.
-            </p>
-            <ul class="slide-list body-copy">
-              <li>HTML</li>
-              <li>SASS</li>
-              <li>CoffeeScript</li>
-              <li>JSON</li>
-              <li>AJAX</li>
-              <li>Responsive</li>
-              <li>Google Maps API</li>
-            </ul>
+        <article class="slide" data-slide="2">
+          <div class="slide-image">
+            <img src="<?php echo IMAGES_PATH; ?>work/wynyard-quarter.jpg" alt="Preview of Wynyard Quarter's website on desktop &amp; tablet">
           </div>
-          <a href="http://www.wynyard-quarter.co.nz/" target="_blank" class="slide-link btn">view site</a>
-        </div>
-      </article>
+          <div class="slide-description">
+            <h2 class="slide-title title separator">Wynyard Quarter</h2>
+            <div class="slide-copy">
+              <p class="body-copy">
+                The aim of this website is to showcase the vibrant precinct of Wynyard Quarter as well as promoting &amp; attracting visitors and communicating on future developments.
+              </p>
+              <ul class="slide-list body-copy">
+                <li>HTML</li>
+                <li>SASS</li>
+                <li>CoffeeScript</li>
+                <li>JSON</li>
+                <li>AJAX</li>
+                <li>Responsive</li>
+                <li>Google Maps API</li>
+              </ul>
+            </div>
+            <a href="http://www.wynyard-quarter.co.nz/" target="_blank" class="slide-link btn">view site</a>
+          </div>
+        </article>
 
-      <article class="slide" data-slide="3">
-        <div class="slide-image">
-          <img src="<?php echo IMAGES_PATH; ?>work/heart-kids.jpg" alt="Preview of WQ Smart's website on desktop &amp; tablet">
-        </div>
-        <div class="slide-description">
-          <h2 class="slide-title title separator">WQ Smart</h2>
-          <div class="slide-copy">
-            <p class="body-copy">
-              Panuku Development Auckland's aim for Wynyard Quarter is to create a mix of residential, retail &amp; commercial development. This website is an engaging way of sharing the progress of implementing sustainability in Wynyard Quarter.
-            </p>
-            <ul class="slide-list body-copy">
-              <li>HTML</li>
-              <li>WebGL</li>
-              <li>SASS</li>
-              <li>CoffeeScript</li>
-              <li>JSON</li>
-              <li>AJAX</li>
-              <li>Responsive</li>
-            </ul>
+        <article class="slide" data-slide="3">
+          <div class="slide-image">
+            <img src="<?php echo IMAGES_PATH; ?>work/heart-kids.jpg" alt="Preview of WQ Smart's website on desktop &amp; tablet">
           </div>
-          <a href="http://www.wqsmart.co.nz/" class="slide-link btn">view site</a>
-        </div>
-      </article>
+          <div class="slide-description">
+            <h2 class="slide-title title separator">WQ Smart</h2>
+            <div class="slide-copy">
+              <p class="body-copy">
+                Panuku Development Auckland's aim for Wynyard Quarter is to create a mix of residential, retail &amp; commercial development. This website is an engaging way of sharing the progress of implementing sustainability in Wynyard Quarter.
+              </p>
+              <ul class="slide-list body-copy">
+                <li>HTML</li>
+                <li>WebGL</li>
+                <li>SASS</li>
+                <li>CoffeeScript</li>
+                <li>JSON</li>
+                <li>AJAX</li>
+                <li>Responsive</li>
+              </ul>
+            </div>
+            <a href="http://www.wqsmart.co.nz/" class="slide-link btn">view site</a>
+          </div>
+        </article>
 
-      <article class="slide" data-slide="4">
-        <div class="slide-image">
-          <img src="<?php echo IMAGES_PATH; ?>work/harrison-grierson.jpg" alt="Preview of Harrison Grierson's website on desktop &amp; phone">
-        </div>
-        <div class="slide-description">
-          <h2 class="slide-title title separator">Harrison Grierson</h2>
-          <div class="slide-copy">
-            <p class="body-copy">
-              Panuku Development Auckland's aim for Wynyard Quarter is to create a mix of residential, retail &amp; commercial development. This website is an engaging way of sharing the progress of implementing sustainability in Wynyard Quarter.
-            </p>
-            <ul class="slide-list body-copy">
-              <li>HTML</li>
-              <li>SASS</li>
-              <li>CoffeeScript</li>
-              <li>AJAX</li>
-              <li>Parallax</li>
-              <li>Mobile</li>
-            </ul>
-            <ul class="slide-list body-copy">
-              <li>W3 Awards Gold</li>
-            </ul>
+        <article class="slide" data-slide="4">
+          <div class="slide-image">
+            <img src="<?php echo IMAGES_PATH; ?>work/harrison-grierson.jpg" alt="Preview of Harrison Grierson's website on desktop &amp; phone">
           </div>
-          <a href="http://www.harrisongrierson.com/" class="slide-link btn">view site</a>
-        </div>
-      </article>
+          <div class="slide-description">
+            <h2 class="slide-title title separator">Harrison Grierson</h2>
+            <div class="slide-copy">
+              <p class="body-copy">
+                Panuku Development Auckland's aim for Wynyard Quarter is to create a mix of residential, retail &amp; commercial development. This website is an engaging way of sharing the progress of implementing sustainability in Wynyard Quarter.
+              </p>
+              <ul class="slide-list body-copy">
+                <li>HTML</li>
+                <li>SASS</li>
+                <li>CoffeeScript</li>
+                <li>AJAX</li>
+                <li>Parallax</li>
+                <li>Mobile</li>
+              </ul>
+              <ul class="slide-list body-copy">
+                <li>W3 Awards Gold</li>
+              </ul>
+            </div>
+            <a href="http://www.harrisongrierson.com/" class="slide-link btn">view site</a>
+          </div>
+        </article>
 
-      <article class="slide" data-slide="5">
-        <div class="slide-image">
-          <img src="<?php echo IMAGES_PATH; ?>work/sovereign-rpm.jpg" alt="Preview of Sovereign RPM's website on desktop">
-        </div>
-        <div class="slide-description">
-          <h2 class="slide-title title separator">Sovereign RPM</h2>
-          <div class="slide-copy">
-            <p class="body-copy">
-              Panuku Development Auckland's aim for Wynyard Quarter is to create a mix of residential, retail &amp; commercial development. This website is an engaging way of sharing the progress of implementing sustainability in Wynyard Quarter.
-            </p>
-            <ul class="slide-list body-copy">
-              <li>HTML</li>
-              <li>SASS</li>
-              <li>CoffeeScript</li>
-              <li>AJAX</li>
-              <li>Interactive</li>
-            </ul>
-            <ul class="slide-list body-copy">
-              <li>NZDM Awards Silver</li>
-            </ul>
+        <article class="slide" data-slide="5">
+          <div class="slide-image">
+            <img src="<?php echo IMAGES_PATH; ?>work/sovereign-rpm.jpg" alt="Preview of Sovereign RPM's website on desktop">
           </div>
-        </div>
-      </article>
+          <div class="slide-description">
+            <h2 class="slide-title title separator">Sovereign RPM</h2>
+            <div class="slide-copy">
+              <p class="body-copy">
+                Panuku Development Auckland's aim for Wynyard Quarter is to create a mix of residential, retail &amp; commercial development. This website is an engaging way of sharing the progress of implementing sustainability in Wynyard Quarter.
+              </p>
+              <ul class="slide-list body-copy">
+                <li>HTML</li>
+                <li>SASS</li>
+                <li>CoffeeScript</li>
+                <li>AJAX</li>
+                <li>Interactive</li>
+              </ul>
+              <ul class="slide-list body-copy">
+                <li>NZDM Awards Silver</li>
+              </ul>
+            </div>
+          </div>
+        </article>
 
-      <article class="slide" data-slide="6">
-        <div class="slide-image">
-          <img src="<?php echo IMAGES_PATH; ?>work/tvnz-media-portal.jpg" alt="Preview of TVNZ Media Portal's website on desktop">
-        </div>
-        <div class="slide-description">
-          <h2 class="slide-title title separator">TVNZ Media Portal</h2>
-          <div class="slide-copy">
-            <p class="body-copy">
-              Panuku Development Auckland's aim for Wynyard Quarter is to create a mix of residential, retail &amp; commercial development. This website is an engaging way of sharing the progress of implementing sustainability in Wynyard Quarter.
-            </p>
-            <ul class="slide-list body-copy">
-              <li>HTML</li>
-              <li>SASS</li>
-              <li>CoffeeScript</li>
-              <li>AJAX</li>
-              <li>Responsive</li>
-            </ul>
+        <article class="slide" data-slide="6">
+          <div class="slide-image">
+            <img src="<?php echo IMAGES_PATH; ?>work/tvnz-media-portal.jpg" alt="Preview of TVNZ Media Portal's website on desktop">
           </div>
-          <a href="http://www.tvnzmediaportal.co.nz/" class="slide-link btn">view site</a>
-        </div>
-      </article>
+          <div class="slide-description">
+            <h2 class="slide-title title separator">TVNZ Media Portal</h2>
+            <div class="slide-copy">
+              <p class="body-copy">
+                Panuku Development Auckland's aim for Wynyard Quarter is to create a mix of residential, retail &amp; commercial development. This website is an engaging way of sharing the progress of implementing sustainability in Wynyard Quarter.
+              </p>
+              <ul class="slide-list body-copy">
+                <li>HTML</li>
+                <li>SASS</li>
+                <li>CoffeeScript</li>
+                <li>AJAX</li>
+                <li>Responsive</li>
+              </ul>
+            </div>
+            <a href="http://www.tvnzmediaportal.co.nz/" class="slide-link btn">view site</a>
+          </div>
+        </article>
+      </div>
     </div>
 
     <div class="slideshow-nav work-element">
@@ -153,13 +155,6 @@
         <svg class="shape-arrow">
           <use xlink:href="#shape-arrow-left"></use>
         </svg>
-      </button>
-
-      <button class="slideshow-all small-hidden">
-        <span class="square"></span>
-        <span class="square"></span>
-        <span class="square"></span>
-        <span class="square"></span>
       </button>
 
       <button class="slideshow-next do-slideshow-next">
