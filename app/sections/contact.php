2def5831ec16e9e176809843ce80eb8c88136893
Celine <celine@methodstudios.co.nz>
2016-03-23 19:59:28 +1300

    Form focus animation

--- app/sections/contact.php
+++ app/sections/contact.php
@@ -5,15 +5,21 @@
 
     <div class="form-group">
       <input type="text" id="name" class="form-input lead" data-validation="required">
-      <label for="name" class="form-label">Name</label>
+      <label for="name" class="form-label" data-content="Name">
+        <span class="form-label-copy">Name</span>
+      </label>
     </div>
     <div class="form-group">
       <input type="email" id="email" class="form-input lead" data-validation="required">
-      <label for="email" class="form-label">Email</label>
+      <label for="email" class="form-label" data-content="Email">
+        <span class="form-label-copy">Email</span>
+      </label>
     </div>
     <div class="form-group">
       <textarea id="message" class="form-textarea lead" rows="3" placeholder="Text..." data-validation="required"></textarea>
-      <label for="message" class="form-label">Message</label>
+      <label for="message" class="form-label" data-content="Message">
+        <span class="form-label-copy">Message</span>
+      </label>
     </div>
     <div class="form-action">
       <button class="btn form-btn">Send</button>
