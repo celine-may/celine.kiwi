53379f92461eb5d1d22a00ebc9451d8860ff464c
Celine Rufener <celine.may@gmail.com>
2016-04-05 21:21:02 +1200

    About -> Copy change

--- app/sections/about.php
+++ app/sections/about.php
@@ -3,10 +3,10 @@
   <div class="section-content">
     <h2 class="about-lead lead separator about-element">A Swiss Touch in Auckland</h2>
     <p class="about-copy body-copy about-element">
-      Being part of the generation that grew up with computers, I've always been into technology. An inclination toward high quality design has naturally grown my passion for front end development.
+      Being part of the generation that grew up with computers, I've always been into technology. An inclination toward high quality design has naturally developed my passion for front end development.
     </p>
     <p class="about-copy body-copy about-element">
-      Kiwi born, I grew up in Switzerland enjoying snowboarding in the Alps and traveling around Europe. In 2014, I decided to move to Auckland to experience the kiwi lifestyle.
+      A Kiwi birth and a Swiss upbringing opened my mind to different ways of thinking. 2014 saw me coming back to New Zealand to experience the kiwi lifestyle...&nbsp;thoroughly loving it!
     </p>
     <button class="about-cta btn small-only do-show-overlay" data-overlay="contact">Let's talk</button>
   </div>
