8bff53a34b6f5fdf7b65ef4aac45eb3b30d7c777
Celine Rufener <celine.may@gmail.com>
2017-05-14 08:40:19 +1200

    Updated skills

--- app/sections/skills.php
+++ app/sections/skills.php
@@ -3,36 +3,52 @@
     <h3 class="skills-title title separator skills-element">Skills</h3>
     <p class="skills-lead lead skills-element">Digital alchemy requires a broad skillset and knowledge.</p>
     <ul class="skills-list skills-element">
+
+      <li class="skill">js</li>
       <li class="skill">html5</li>
       <li class="skill">css3</li>
-      <li class="skill">js</li>
-      <li class="skill">php</li>
-      <li class="skill">yii</li>
+
+      <li class="skill">reactjs</li>
+      <li class="skill">es6</li>
       <li class="skill">ror</li>
-      <li class="skill">jquery</li>
-      <li class="skill">sass</li>
-      <li class="skill">coffee</li>
-      <li class="skill">json</li>
+
       <li class="skill">ajax</li>
-      <li class="skill">webgl</li>
-      <li class="skill">angularjs</li>
-      <li class="skill">less</li>
-      <li class="skill">Responsive</li>
-      <li class="skill">Google Maps</li>
-      <li class="skill">SQL</li>
+      <li class="skill">jest</li>
+      <li class="skill">webpack</li>
+
+      <li class="skill">ui/ux</li>
+      <li class="skill">material ui</li>
+      <li class="skill">responsive</li>
+
+
+      <li class="skill">adobe xd</li>
+      <li class="skill">adobe ps</li>
+      <li class="skill">adobe ai</li>
+
+      <li class="skill">coffee</li>
+      <li class="skill">sass</li>
+      <li class="skill">json</li>
+
       <li class="skill">git</li>
-      <li class="skill">grunt</li>
-      <li class="skill">gulp</li>
-      <li class="skill">Command lines</li>
-      <li class="skill">sublime text</li>
-      <li class="skill">photoshop</li>
-      <li class="skill">illustrator</li>
-      <li class="skill">fireworks</li>
       <li class="skill">npm</li>
-      <li class="skill">Bootstrap</li>
+      <li class="skill">command lines</li>
+
+      <li class="skill">webgl</li>
       <li class="skill">parallax</li>
-      <li class="skill">Mobile APP</li>
-      <li class="skill">Local storage</li>
+      <li class="skill">mobile app</li>
+
+
+      <li class="skill">php</li>
+      <li class="skill">sql</li>
+      <li class="skill">jquery</li>
+
+      <li class="skill">bootstrap</li>
+      <li class="skill">angularjs</li>
+      <li class="skill">local storage</li>
+
+      <li class="skill">sublime text</li>
+      <li class="skill">gulp</li>
+      <li class="skill">grunt</li>
     </ul>
     <div class="skills-nav skills-element"></div>
     <button class="skills-btn btn skills-element do-show-overlay" data-overlay="contact">Let's talk</button>
